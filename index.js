fdd92262198796dc3b69da3290ce039f289d54b7
YojiSato65 <yojisato65@gmail.com>
2022-03-02 17:36:16 +0200

    Progress1

--- index.js
+++ index.js
@@ -1,8 +1,34 @@
-fetch("https://api.pexels.com/v1/search?query=[your-query]", {
-    headers: {Authorization: "Bearer [563492ad6f91700001000001019e8da5373c467d99250c3231c5499d]"}
+let imageWrapper = document.querySelectorAll('svg')
+let images = document.getElementsByTagName('img')
+let test = document.querySelector('#test')
+let card = document.querySelectorAll('#card')
 
-}) 
+// console.log(imageWrapper)
+function loadImages()
+{
+    fetch("https://api.pexels.com/v1/search?query=cars", {
+        headers: { Authorization: "Bearer 563492ad6f91700001000001019e8da5373c467d99250c3231c5499d" }
 
-.then(response =>.json ())
+    })
 
+        .then(response => response.json())
+        .then(data =>
+        {
+            console.log(data)
+            for (let dat of data.photos)
+            // for (let i = 0; i < images.length; i++)
+            {
+                card.innerHTML = `<div>${data.photos}</div>`
+                // test.photos[0].src.large
+                // images[i] = images.photos[i].src.large
+                // imageWrapper[i].style.display = 'none'
+
+                // imageWrapper.innerHTML = `
+                // <div>
+                // <img src='${images.photos[i].src.medium}'>
+                // </div>
+                // `
+            }
+        })
+}
 
