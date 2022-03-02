43306b2583f8ae48dade581c4e1ce1ff09ee8096
Fabian-Boettges <95299029+Fabian-Boettges@users.noreply.github.com>
2022-03-02 14:50:53 +0100

    fetch attempt

---
+++ index.js
@@ -0,0 +1,8 @@
+fetch("https://api.pexels.com/v1/search?query=[your-query]", {
+    headers: {Authorization: "Bearer [563492ad6f91700001000001019e8da5373c467d99250c3231c5499d]"}
+
+}) 
+
+.then(response =>.json ())
+
+
